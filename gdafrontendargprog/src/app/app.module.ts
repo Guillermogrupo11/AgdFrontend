71b2948f93e6777b62beb19507a73acea51bb5e9
Guillermo <guillermo.arena@gmail.com>
2022-10-05 21:42:10 -0300

    creando componentes

--- gdafrontendargprog/src/app/app.module.ts
+++ gdafrontendargprog/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    FooterComponent,
+    HomeComponent,
+    HysComponent,
+    ProyectoComponent,
+    LoginComponent,
+    LogoApComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,

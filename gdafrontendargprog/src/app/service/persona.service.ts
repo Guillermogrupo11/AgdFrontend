f444e8709694c823a4c4175a26cc88687e63d97b
Guillermo <guillermo.arena@gmail.com>
2022-10-11 14:40:41 -0300

    skill service

--- gdafrontendargprog/src/app/service/persona.service.ts
+++ gdafrontendargprog/src/app/service/persona.service.ts
@@ -9,11 +9,11 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+  URL = environment.URL + 'personas/';
 
   constructor(private http: HttpClient) { }
 
- public getPersona(): Observable<persona>{
-   return this.http.get<persona>(this.URL+ 'traer/perfil');
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+ 'traer/perfil');
   }
 }

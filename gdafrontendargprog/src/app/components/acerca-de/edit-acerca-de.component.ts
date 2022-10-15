5e928a663548db374546aabbf6599a7503d574de
Guillermo <guillermo.arena@gmail.com>
2022-10-15 18:05:11 -0300

    edit acerca de html y ts

--- gdafrontendargprog/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ gdafrontendargprog/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
 
 @Component({
   selector: 'app-edit-acerca-de',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-acerca-de.component.css']
 })
 export class EditAcercaDeComponent implements OnInit {
-
+persona: persona = null;
   constructor() { }
 
   ngOnInit(): void {
   }
+  onUpdate(){}
+  uploadImage($event: any){}
 
 }

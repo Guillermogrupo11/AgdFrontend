17d7530ca0c1de36da07edd3bea2573014f9ec9e
Guillermo <guillermo.arena@gmail.com>
2022-10-15 18:13:59 -0300

    Update edit-acerca-de.component.ts

--- gdafrontendargprog/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ gdafrontendargprog/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-edit-acerca-de',
@@ -8,9 +10,19 @@ import { persona } from 'src/app/model/persona.model';
 })
 export class EditAcercaDeComponent implements OnInit {
 persona: persona = null;
-  constructor() { }
+  constructor(private activatedRouter: ActivatedRoute , private personaService: PersonaService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.personaService.detail(id).subscribe(
+      data =>{
+        this.persona = data;
+      }, err =>{
+         alert("Error al modificar");
+         this.router.navigate(['']);
+      }
+    )
   }
   onUpdate(){}
   uploadImage($event: any){}
